9ea1349297e954c4333415250b29143678ab49bc
ctrl-alt-delor <none@none>
2025-04-05 13:43:39 +0100

    experimenting with querrying tab-groups

--- popup2.js
+++ popup2.js
@@ -1,18 +1,21 @@
 async function show_tabGroups(){
-    const groups = await chrome.windows.getAll({
-    });
+    const groups = await chrome.tabGroups.query({});
     
     const template = document.getElementById('li_template');
     const elements = new Set();
+    const section_heading=document.createElement('h2');
+    section_heading.textContent='Tab Groups';
+    elements.add(section_heading);
     for (const group of groups) {
-        const element = template.content.firstElementChild.cloneNode(true);  
+        const element = template.content.cloneNode(true);
+        element.querySelector('.title').textContent = group.title;
         element.querySelector('.content').textContent = JSON.stringify(group);
-        element.querySelector('a').addEventListener('click', async () => {        
-        });
         elements.add(element);
     }
     document.querySelector('ul#output-list').append(...elements);
 }
+
+
     
 function enable_button(){
     const button = document.querySelector('button');
@@ -23,3 +26,4 @@ function enable_button(){
 
 await show_tabGroups();
 enable_button();
+

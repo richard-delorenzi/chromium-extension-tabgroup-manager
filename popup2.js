08e7635d4b1598e8240a2e67b68ed3092d64117d
ctrl-alt-delor <none@none>
2025-04-07 12:06:29 +0100

    refactor

--- popup2.js
+++ popup2.js
@@ -1,18 +1,18 @@
-async function show_tabGroups(){
-    const groups = await chrome.tabGroups.query({});
-    
+function show(datas){
+    const template = document.getElementById('template');
     const li_template = document.getElementById('li_template');
-    const elements = [];
-    const section_heading=document.createElement('h2');
-    section_heading.textContent='Tab Groups';
-    elements.push(section_heading);
-    for (const group of groups) {
+    
+    const section = template.content.cloneNode(true);
+    section.querySelector('#heading').textContent = "tab datas";
+    const output_list=section.querySelector('ul#output-list');
+    for (const data of datas) {
         const element = li_template.content.cloneNode(true);
-        element.querySelector('#title').textContent = group.title;
-        element.querySelector('#content').textContent = JSON.stringify(group);
-        elements.push(element);
+        element.querySelector('#title').textContent = data.title;
+        element.querySelector('#content').textContent = JSON.stringify(data);
+        output_list.append(element);
     }
-    document.querySelector('ul#output-list').append(...elements);
+    
+    document.querySelector('#output').append(section);
 }
 
 
@@ -23,6 +23,9 @@ function enable_button(){
         document.querySelector('p#notes').textContent = "you clicked the button";
     });
 }
+
+const groups = await chrome.tabGroups.query({});
+show(groups);
 
 await show_tabGroups();
 enable_button();
